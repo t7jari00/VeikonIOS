c0f8a83dd2c76b155683ea84401d7dbe136133b2
t7jari00 <t7jari00@students.oamk.fi>
2019-08-27 18:23:44 +0300

    start

---
+++ Lab1Teht2/Lab1Teht2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab1Teht2
+//
+//  Created by Risto Jääskelä on 27/08/2019.
+//  Copyright © 2019 Risto Jääskelä. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
