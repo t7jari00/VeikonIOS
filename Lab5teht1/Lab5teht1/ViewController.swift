f0fd454a7c2583565c80e166b054f3de2b3c030e
janne <t7tyja00@students.oamk.fi>
2019-09-25 13:26:25 +0300

    jumps

---
+++ Lab5teht1/Lab5teht1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab5teht1
+//
+//  Created by Janne Tyni on 25/09/2019.
+//  Copyright © 2019 Janne Tyni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
